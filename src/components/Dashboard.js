6f059612f4bb366b0cd21c76c2105aae2d52d08c
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-27 23:10:59 -0500

    update Panel props to render appointment data from api server

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,28 +6,33 @@ import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
 
-
+import {
+ getTotalInterviews,
+ getLeastPopularTimeSlot,
+ getMostPopularDay,
+ getInterviewsPerDay
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -88,8 +93,8 @@ class Dashboard extends Component {
         <Panel
           key={panel.id}
           label={panel.label}
-          value={panel.value}
-          onSelect={event => this.selectPanel(panel.id)}
+          value={panel.getValue(this.state)}
+          onSelect={() => this.selectPanel(panel.id)}
         />
       ));
     
